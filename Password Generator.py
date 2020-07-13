b42e2a9c697132b515c4be8fef3c6b51aaa9eb0d
RosJes <isaksson.jessica@gmail.com>
2020-07-13 13:19:53 +0200

    password generator without conditions

--- Password Generator.py
+++ Password Generator.py
@@ -1,2 +1,13 @@
+import random
 '''step 1: generate an array of chars without conditions'''
 '''step 2: add conditions to array of chars'''
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
+password_length=input('Password length?')
+converted_length=int(password_length)
+password=''
+for x in range(converted_length):
+    password += random.choice(chars)
+
+print(password)
+
