b3f23776044c521d99d0e6877c358cb7a4fbf98a
RosJes <isaksson.jessica@gmail.com>
2020-07-13 14:11:28 +0200

    Regex added

--- Password Generator.py
+++ Password Generator.py
@@ -1,4 +1,5 @@
 import random
+import re
 '''step 1: generate an array of chars without conditions'''
 '''step 2: add conditions to array of chars'''
 
@@ -11,8 +12,21 @@ allow_numbers=input('Allow numbers?')
 allow_spec_char=input('Allow special character?')
 password=''
 for x in range(converted_length):
-    password += random.choice(chars)
-    '''Reg Ex check: upper, lower osv'''
+    if allow_lower == 'n':
+        x = re.findall("[A-Z\d @_!#$%^&*()<>?/\|}{~:]", chars)
+        password += random.choice(x)
+    if allow_upper == 'n':
+        x = re.findall("[a-z\d @_!#$%^&*()<>?/\|}{~:]", chars)
+        password += random.choice(x)
+    if allow_numbers == 'n':
+        x = re.findall("[a-z @_!#$%^&*()<>?/\|}{~:]", chars)
+        password += random.choice(x)
+    if allow_spec_char == 'n':
+        x = re.findall("[A-z\d ]", chars)
+        password += random.choice(x)
+
+'''Reg Ex check: upper, lower osv'''
+'''fix logical problem with correct if statements'''
 
 print(f'Your generated password is: {password}')
 
