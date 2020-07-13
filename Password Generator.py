59c0793750f6a2a254841de2481ba7dea7c5e9a8
RosJes <isaksson.jessica@gmail.com>
2020-07-13 13:45:04 +0200

    password generator modified

--- Password Generator.py
+++ Password Generator.py
@@ -5,9 +5,14 @@ import random
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
 password_length=input('Password length?')
 converted_length=int(password_length)
+allow_upper=input('Allow uppercase?')
+allow_lower=input('Allow lowercase?')
+allow_numbers=input('Allow numbers?')
+allow_spec_char=input('Allow special character?')
 password=''
 for x in range(converted_length):
     password += random.choice(chars)
+    '''Reg Ex check: upper, lower osv'''
 
-print(password)
+print(f'Your generated password is: {password}')
 
