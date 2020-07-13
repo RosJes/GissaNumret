81ff409f23a211d2f0b02f7948996bbec7f28ba4
RosJes <isaksson.jessica@gmail.com>
2020-07-13 14:26:34 +0200

    Resolving correct if statements

--- Password Generator.py
+++ Password Generator.py
@@ -6,23 +6,15 @@ import re
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
 password_length=input('Password length?')
 converted_length=int(password_length)
-allow_upper=input('Allow uppercase?')
-allow_lower=input('Allow lowercase?')
-allow_numbers=input('Allow numbers?')
-allow_spec_char=input('Allow special character?')
 password=''
+allow_upper=input('Allow uppercase?')
+allow_numbers=input('Allow numbers?')
 for x in range(converted_length):
-    if allow_lower == 'n':
-        x = re.findall("[A-Z\d @_!#$%^&*()<>?/\|}{~:]", chars)
+    if allow_upper=='y':
+        x = re.findall("[A-z @_!#$%^&*()<>?/\|}{~:]", chars)
         password += random.choice(x)
-    if allow_upper == 'n':
-        x = re.findall("[a-z\d @_!#$%^&*()<>?/\|}{~:]", chars)
-        password += random.choice(x)
-    if allow_numbers == 'n':
+    else:
         x = re.findall("[a-z @_!#$%^&*()<>?/\|}{~:]", chars)
-        password += random.choice(x)
-    if allow_spec_char == 'n':
-        x = re.findall("[A-z\d ]", chars)
         password += random.choice(x)
 
 '''Reg Ex check: upper, lower osv'''
