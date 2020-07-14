9d9e642026a5479e83f463c7479ac811717d8ce4
RosJes <isaksson.jessica@gmail.com>
2020-07-14 20:39:42 +0200

    First method implemented

--- Tre_i_rad.py
+++ Tre_i_rad.py
@@ -3,12 +3,32 @@
 '''what is the game structure?'''
 '''figure out user input, what happens?'''
 '''What classes do you need? player etc.'''
+'''Learn classes,methods etc.'''
 
-row,cols=(3,3)
+'''display field'''
+
+'''row,cols=(3,3)
 arr=[[0]*cols]*row
 
 for row in arr:
     print(row)
+'''
+def draw_game_board():
+    print('Hello')
+
+draw_game_board()
+'''display field'''
+'''create method'''
+'''user input'''
+'''computer input'''
+'''clear terminal'''
+'''make logic with koordinations ex 1,2'''
+'''the user changes the zero to X with specification
+the user input can ba f example this:
+write destination X= 1
+write destination Y=2
+the position of X is now [1,2]
+    '''
 
 
 
