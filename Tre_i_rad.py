3b1fd03ef974e670541ef9ba654ef73b9da41524
RosJes <isaksson.jessica@gmail.com>
2020-07-14 17:54:48 +0200

    playing field example

---
+++ Tre_i_rad.py
@@ -0,0 +1,14 @@
+
+'''make a play field '''
+'''what is the game structure?'''
+'''figure out user input, what happens?'''
+'''What classes do you need? player etc.'''
+
+row,cols=(3,3)
+arr=[[0]*cols]*row
+
+for row in arr:
+    print(row)
+
+
+
