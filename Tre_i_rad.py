e012cfb29c4abc073e74cf0a2f61a0815efd8404
RosJes <isaksson.jessica@gmail.com>
2020-07-18 12:02:02 +0200

    game without errormessage conditions

--- Tre_i_rad.py
+++ Tre_i_rad.py
@@ -1,12 +1,8 @@
 import random
 import os
 
-'''Solution to one loop:
-is here switches to true and error message to false
-if errormessage is false the loop will jump to the beginning, well in the beginning 
-there is an if condition: is here true and errormessage komp is now true since the switch is back to normal
-test the while loop, why doesn't it break when one of the statements switch to false?
-'''
+'''What is not working: Clear screen
+when computer choose a destination or you do it you dont get a second chance'''
 rows, cols = (3, 3)
 errormessageyou = False;
 errormessagecomp = False
@@ -25,18 +21,12 @@ def play_game():
         print(i, row)
         if (i >= 3):
             i = 0
-    while (not errormessagecomp or not errormessageyou or play_again):
-        if (errormessagecomp is True and is_here is True):
-            comp_movex = random.randint(0, 2)
-            comp_movey = random.randint(0, 2)
-            arr[int(comp_movex)][int(comp_movey)] = 5
-
+    while (play_again):
         os.system('cls')
         x_guess = input('Select destination x')
         y_guess = input('Select destination y')
         if arr[int(x_guess)][int(y_guess)] != 0:
             print("The destination is occupied")
-            errormessageyou is True
 
         else:
             arr[int(x_guess)][int(y_guess)] = 1
@@ -44,8 +34,7 @@ def play_game():
         comp_movey = random.randint(0, 2)
         print(comp_movey, comp_movex)
         if arr[int(comp_movex)][int(comp_movey)] != 0:
-            print('computer fucked up')
-            errormessagecomp is True
+            print('computer fucked up it chose the same coordinates as you ')
         else:
             arr[int(comp_movex)][int(comp_movey)] = 5
 
@@ -57,6 +46,8 @@ def play_game():
         wincheck7 = arr[0][2] + arr[1][2] + arr[2][2]
         wincheck3 = arr[0][0] + arr[1][1] + arr[2][2]
         wincheck8 = arr[0][2] + arr[1][2] + arr[2][0]
+        wincheck9=arr[0][2] + arr[1][1] + arr[2][0]
+
         print('1   2   3')
         print('------------')
         for row in arr:
@@ -64,7 +55,7 @@ def play_game():
             print(i, row)
             if (i >= 3):
                 i = 0
-        if wincheck1 == 3 or wincheck2 == 3 or wincheck3 == 3 or wincheck4 == 3 or wincheck5 == 3 or wincheck6 == 3 or wincheck7 == 3 or wincheck8 == 3:
+        if wincheck1 == 3 or wincheck2 == 3 or wincheck3 == 3 or wincheck4 == 3 or wincheck5 == 3 or wincheck6 == 3 or wincheck7 == 3 or wincheck8 == 3 or wincheck9==3:
             print('you win')
             user_input = input('play again?y/n')
             if user_input == 'y':
@@ -74,11 +65,10 @@ def play_game():
             else:
                 break;
 
-        elif wincheck1 == 15 or wincheck2 == 15 or wincheck3 == 15 or wincheck4 == 15 or wincheck5 == 15 or wincheck6 == 15 or wincheck7 == 15 or wincheck8 == 15:
+        elif wincheck1 == 15 or wincheck2 == 15 or wincheck3 == 15 or wincheck4 == 15 or wincheck5 == 15 or wincheck6 == 15 or wincheck7 == 15 or wincheck8 == 15 or wincheck9==15:
             print('computer win')
             user_input = input('play again?y/n')
             if user_input == 'y':
-                play_again is True
                 arr = [[0 for i in range(cols)]
                        for j in range(rows)]
             else:
@@ -86,7 +76,5 @@ def play_game():
 
 
 play_game()
-
-
 
 
